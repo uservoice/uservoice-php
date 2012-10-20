55606ef786ddcb8091bdd57d2fbd50783acf23b5
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-10-19 19:22:36 -0700

    Use same-line curly brackets

--- uservoice/client.php
+++ uservoice/client.php
@@ -10,8 +10,7 @@ class UserVoiceHelper
 	const SUBDOMAIN = "acme";
 
 
-	public static function CreateTicketNote($ticket_id,$msg)
-	{
+	public static function CreateTicketNote($ticket_id,$msg) {
 		$oauthObject=self::GetOauth(true);
 
 		$res = self::MakeRequest($oauthObject,'https://'.self::SUBDOMAIN.'.uservoice.com/api/v1/tickets/'.$ticket_id.'/notes.json',array("note[text]" => $msg),"POST");
@@ -19,8 +18,7 @@ class UserVoiceHelper
 		return $res;
 	}
 
-	public static function GetTicketsBySearch($query)
-	{
+	public static function GetTicketsBySearch($query) {
 		$oauthObject=self::GetOauth();
 
 		$res = self::MakeRequest($oauthObject, 'http://'.self::SUBDOMAIN.'.uservoice.com/api/v1/tickets/search.json', array('query' => $query));
@@ -28,8 +26,7 @@ class UserVoiceHelper
 		return $res->tickets;
 	}
 
-	public static function AuthorizeBySSO($user_data)
-	{
+	public static function AuthorizeBySSO($user_data) {
 		$oauthObject=self::GetOauth();
 
 		$token = self::MakeRequest($oauthObject, 'http://'.self::SUBDOMAIN.'.uservoice.com/api/v1/oauth/request_token.json', array());
@@ -47,16 +44,14 @@ class UserVoiceHelper
 		return $res;
 	}
 
-	public static function GetOauth($use_admin=false)
-	{
+	public static function GetOauth($use_admin=false) {
 		$oauthObject = new OAuthSimple(self::OAUTH_CONSUMER_KEY,self::OAUTH_CONSUMER_SECRET);
 		/*$oauthObject->sig = array(
 			'consumer_key'     => self::OAUTH_CONSUMER_KEY,
 			'shared_secret'    => self::OAUTH_CONSUMER_SECRET);
 		*/
 
-		if($use_admin)
-		{
+		if($use_admin) {
 			$oauthObject->setTokensAndSecrets(array(
 				"access_token"=>self::ADMIN_OAUTH_TOKEN,
 				"access_secret"=> self::ADMIN_OAUTH_SECRET
@@ -66,8 +61,7 @@ class UserVoiceHelper
 		return $oauthObject;
 	}
 
-	public static function MakeRequest($oauthObject, $apiUrl, $params, $action="GET")
-	{
+	public static function MakeRequest($oauthObject, $apiUrl, $params, $action="GET") {
 		$oauthObject->setAction($action);
 
 		$result = $oauthObject->sign(array(
@@ -81,8 +75,7 @@ class UserVoiceHelper
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_URL, $result['signed_url']);
 
-		if($action=="POST")
-		{
+		if($action=="POST") {
 			curl_setopt($ch,CURLOPT_POST,true);
 			curl_setopt($ch,CURLOPT_POSTFIELDS,$params);
 		}
