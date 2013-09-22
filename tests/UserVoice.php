7316f298fa34b97fc316eba26e434dbba9f7b98e
Lars Vierbergen <vierbergenlars@gmail.com>
2013-09-22 14:07:43 +0200

    Migrate to simpletest/simpletest for unit tests

--- tests/UserVoice.php
+++ tests/UserVoice.php
@@ -1,5 +1,5 @@
 <?php
-include_once(dirname(__FILE__).'/../vendor/vierbergenlars/simpletest/autorun.php');
+include_once(dirname(__FILE__).'/../vendor/simpletest/simpletest/autorun.php');
 require_once('vendor/autoload.php');
 require_once('tests/sso_test.php');
 require_once('tests/client_test.php');
